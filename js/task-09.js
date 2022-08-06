f1b5f9e4371e1a9c6f3909822a59b45c7ede9941
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-06 20:01:41 +0300

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const btnRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+btnRef.addEventListener("click", onChangeColorBtn);
+
+function onChangeColorBtn() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = bodyRef.style.backgroundColor;
+}
