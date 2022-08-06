0f095a4cc3cfb2753eb5bcb5d28f5d3f9464ff8a
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-06 18:22:36 +0300

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+const infoRef = {};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const inputFirstName = formRef.elements[0].name;
+  const inputSecondName = formRef.elements[1].name;
+  const inputFirstValue = formRef.elements[0].value;
+  const inputSecondValue = formRef.elements[1].value;
+  if (inputFirstValue === "" || inputSecondValue === "") {
+    alert("Все поля должны быть заполнены!");
+  } else {
+    infoRef[inputFirstName] = inputFirstValue;
+    infoRef[inputSecondName] = inputSecondValue;
+    console.log(infoRef);
+    event.target.reset();
+  }
+}
